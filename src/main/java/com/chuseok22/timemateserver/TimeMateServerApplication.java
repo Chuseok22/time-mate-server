89c8935887c172a26d203f205c4ece64c8e3151f
Chuseok22 <bjh59629@naver.com>
2025-09-30 13:44:27 +0900

    project init

---
+++ src/main/java/com/chuseok22/timemateserver/TimeMateServerApplication.java
@@ -0,0 +1,13 @@
+package com.chuseok22.timemateserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeMateServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TimeMateServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/chuseok22/timemateserver/TimeMateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuseok22.timemateserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeMateServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
