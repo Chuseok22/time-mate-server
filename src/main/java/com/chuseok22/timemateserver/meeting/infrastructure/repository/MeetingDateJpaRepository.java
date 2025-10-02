e395c807ab79bb7422967315b722f82723f15cfb
Chuseok22 <bjh59629@naver.com>
2025-10-02 11:33:23 +0900

    모임_방_생성_로직_구현 : feat : jpa repository 코드 추가 https://github.com/Chuseok22/time-mate-server/issues/7

--- src/main/java/com/chuseok22/timemateserver/meeting/infrastructure/repository/MeetingDateJpaRepository.java
+++ src/main/java/com/chuseok22/timemateserver/meeting/infrastructure/repository/MeetingDateJpaRepository.java
@@ -1,9 +1,13 @@
 package com.chuseok22.timemateserver.meeting.infrastructure.repository;
 
 import com.chuseok22.timemateserver.meeting.infrastructure.entity.MeetingDate;
+import com.chuseok22.timemateserver.meeting.infrastructure.entity.MeetingRoom;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MeetingDateJpaRepository extends JpaRepository<MeetingDate, UUID> {
+
+  List<MeetingDate> findAllByMeetingRoom(MeetingRoom room);
 
 }
