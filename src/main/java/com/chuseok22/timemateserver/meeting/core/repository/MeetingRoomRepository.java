440f158175c93663af6f5fa06ccc0ad08e2870de
Chuseok22 <bjh59629@naver.com>
2025-10-02 11:33:10 +0900

    모임_방_생성_로직_구현 : feat : core repository 코드 추가 https://github.com/Chuseok22/time-mate-server/issues/7

--- src/main/java/com/chuseok22/timemateserver/meeting/core/repository/MeetingRoomRepository.java
+++ src/main/java/com/chuseok22/timemateserver/meeting/core/repository/MeetingRoomRepository.java
@@ -1,5 +1,11 @@
 package com.chuseok22.timemateserver.meeting.core.repository;
 
+import com.chuseok22.timemateserver.meeting.infrastructure.entity.MeetingRoom;
+import java.util.UUID;
+
 public interface MeetingRoomRepository {
 
+  MeetingRoom save(MeetingRoom meetingRoom);
+
+  MeetingRoom findById(UUID id);
 }
