d2c5c51644f7adc080564eb94277c4a1dbfe4fb9
Chuseok22 <bjh59629@naver.com>
2025-10-04 02:18:42 +0900

    방_참가_로직_개발 : feat : 참석 가능 시간 service 클래스 생성 https://github.com/Chuseok22/time-mate-server/issues/9

---
+++ src/main/java/com/chuseok22/timemateserver/meeting/application/service/AvailabilityTimeServiceImpl.java
@@ -0,0 +1,7 @@
+package com.chuseok22.timemateserver.meeting.application.service;
+
+import com.chuseok22.timemateserver.meeting.core.service.AvailabilityTimeService;
+
+public class AvailabilityTimeServiceImpl implements AvailabilityTimeService {
+
+}
