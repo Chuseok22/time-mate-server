b0ad01448c9ff2e17867110f259534022135e5fe
Chuseok22 <bjh59629@naver.com>
2025-10-05 01:01:16 +0900

    참가자별_참석_가능_시간_선택_api_개발 : feat : 참가자별 참석 가능 시간 설정 로직 구현 https://github.com/Chuseok22/time-mate-server/issues/17

--- src/main/java/com/chuseok22/timemateserver/meeting/application/service/AvailabilityTimeServiceImpl.java
+++ src/main/java/com/chuseok22/timemateserver/meeting/application/service/AvailabilityTimeServiceImpl.java
@@ -1,7 +1,48 @@
 package com.chuseok22.timemateserver.meeting.application.service;
 
+import com.chuseok22.timemateserver.meeting.application.dto.request.AvailabilityTimeRequest;
+import com.chuseok22.timemateserver.meeting.application.dto.request.UpsertAvailabilityRequest;
+import com.chuseok22.timemateserver.meeting.core.repository.AvailabilityTimeRepository;
+import com.chuseok22.timemateserver.meeting.core.repository.MeetingDateRepository;
+import com.chuseok22.timemateserver.meeting.core.repository.ParticipantRepository;
 import com.chuseok22.timemateserver.meeting.core.service.AvailabilityTimeService;
+import com.chuseok22.timemateserver.meeting.infrastructure.entity.AvailabilityTime;
+import com.chuseok22.timemateserver.meeting.infrastructure.entity.MeetingDate;
+import com.chuseok22.timemateserver.meeting.infrastructure.entity.Participant;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class AvailabilityTimeServiceImpl implements AvailabilityTimeService {
 
+  private final AvailabilityTimeRepository availabilityTimeRepository;
+  private final MeetingDateRepository meetingDateRepository;
+  private final ParticipantRepository participantRepository;
+
+  @Override
+  @Transactional
+  public void setAvailabilityTime(UpsertAvailabilityRequest request) {
+    Participant participant = participantRepository.findById(request.getParticipantId());
+    deleteAllByParticipant(participant);
+    request.getAvailabilityTimeRequests()
+        .forEach(req -> saveAvailabilityTimes(participant, req));
+  }
+
+  private void deleteAllByParticipant(Participant participant) {
+    availabilityTimeRepository.deleteAllByParticipant(participant);
+  }
+
+  private void saveAvailabilityTimes(Participant participant, AvailabilityTimeRequest request) {
+    MeetingDate meetingDate = meetingDateRepository.findByDate(request.getDate());
+    List<AvailabilityTime> availabilityTimes = request.getTimeSlots().stream()
+        .map(timeslot -> AvailabilityTime.create(participant, meetingDate, timeslot))
+        .collect(Collectors.toList());
+    availabilityTimeRepository.saveAll(availabilityTimes);
+  }
 }
--- src/main/java/com/chuseok22/timemateserver/meeting/core/service/AvailabilityTimeService.java
+++ src/main/java/com/chuseok22/timemateserver/meeting/core/service/AvailabilityTimeService.java
@@ -1,5 +1,8 @@
 package com.chuseok22.timemateserver.meeting.core.service;
 
+import com.chuseok22.timemateserver.meeting.application.dto.request.UpsertAvailabilityRequest;
+
 public interface AvailabilityTimeService {
 
+  void setAvailabilityTime(UpsertAvailabilityRequest request);
 }
