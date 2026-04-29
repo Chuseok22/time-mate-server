da624f9ef98d05b537d96c590e35d31109602b67
Baek Jihoon <133375485+Chuseok22@users.noreply.github.com>
2026-04-29 23:52:50 +0900

    로그인_기능_추가 : feat : ErrorCode JWT/User/Auth 관련 에러 추가 https://github.com/Chuseok22/meet-time-server/issues/33

--- src/main/java/com/chuseok22/timemateserver/common/core/exception/ErrorCode.java
+++ src/main/java/com/chuseok22/timemateserver/common/core/exception/ErrorCode.java
@@ -16,6 +16,11 @@ public enum ErrorCode {
 
   ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근이 거부되었습니다."),
 
+  // JWT
+  TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
+
+  INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+
   // Meeting Room
 
   MEETING_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "Meeting Room을 찾을 수 없습니다."),
@@ -32,6 +37,9 @@ public enum ErrorCode {
 
   TIME_SLOT_NOT_FOUND(HttpStatus.NOT_FOUND, "LocalTime에 맞는 TimeSlot이 없습니다."),
 
+  // User
+  USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+
   // Participant
 
   PARTICIPANT_NOT_FOUND(HttpStatus.NOT_FOUND, "참가자를 찾을 수 없습니다."),
@@ -39,6 +47,10 @@ public enum ErrorCode {
   DUPLICATE_USERNAME(HttpStatus.CONFLICT, "이미 존재하는 이름입니다."),
 
   INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
+
+  ROOM_DELETE_FORBIDDEN(HttpStatus.FORBIDDEN, "방 삭제 권한이 없습니다."),
+
+  PARTICIPANT_DELETE_FORBIDDEN(HttpStatus.FORBIDDEN, "참가자 삭제 권한이 없습니다."),
 
   // Telegram
 
